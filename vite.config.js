6a8a78bb58ebc9ad58380a9f687ea93df3094f1e
Alson Dabo <alsondab9@gmail.com>
2025-06-27 14:57:02 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || '/Portfolio-in-React-with-Vite-js-and-tailwindcss',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
